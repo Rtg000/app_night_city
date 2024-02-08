8dc0abb0009dbc5db9c4f1677995fbf0c3498482
Rtg000 <rtergom310@g.educaand.es>
2024-02-08 12:22:21 +0000

    pages corpos gangs y fixers añadidas semiestable_v2

--- src/app/public/corpos/layout.tsx
+++ src/app/public/corpos/layout.tsx
@@ -2,7 +2,7 @@ import { Menu } from "@/components/Navbar";
 import { IChildren } from "@/interfaces/IChildren";
 import { ILink } from "@/interfaces/ILink";
 
-export default function GangsLayout({children}: IChildren){
+export default function CorposLayout({children}: IChildren){
     return(
         <main>
             {children}
