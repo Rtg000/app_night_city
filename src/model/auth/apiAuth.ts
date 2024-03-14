6a9a4830f91fb6a829a144ea9fb003e902fe6910
Ubuntu <ubuntu@ip-172-31-10-253.ec2.internal>
2024-03-14 02:19:45 +0000

    llamada api auth login funcional estable

--- src/model/auth/apiAuth.ts
+++ src/model/auth/apiAuth.ts
@@ -7,9 +7,15 @@ const apiAuth = {
 
     login: async (login: ILogin): Promise<IRespLogin> => {
         const ruta = `${apiBD}/auth/login`;
-        const {data} = await axios.post(`${ruta}`,{...login})
+        const {data} = await axios.post(`${ruta}`,login)
         return data
-    }
+    },
+
+    // register: async (register: IRegister): Promise<IRespRegister> => {
+    //     const ruta = `${apiBD}/auth/register`;
+    //     const {data} = await axios.post(`${ruta}`,register)
+    //     return data
+    // }
 
 }
 
