3493b42be9f28ade7ebe217e8f6b40d2ef52b862
Ubuntu <ubuntu@ip-172-31-10-253.ec2.internal>
2024-03-14 02:10:16 +0000

    home reestructurado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
+import { ILink } from "@/interfaces/ILink";
+import { Menu } from "@/components/commons/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,12 +12,28 @@ export const metadata: Metadata = {
   description: "Night City Web",
 };
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function HomeLayout({children}: { children: React.ReactNode }) {
+  
+  const links:ILink[] = [
+    {name: "CYBERWARE", href: "/public/cyberware"},
+    {name: "CHOOMBAS", href: "/public/choombas"},
+    {name: "DISTRITOS", href: "/public/distritos"},
+    {name: "FIXERS", href: "/public/fixers"},
+    {name: "GANGS", href: "/public/gangs"},
+    {name: "CORPOS", href: "/public/corpos"},
+    // {name: "TEST", href: "/public/test"}
+  ];
+
   return (
     <html lang="es" className='dark'>
       <body>
         <Providers>
-          {children}
+          <header>
+            <Menu links = {links}/>
+          </header>
+          <main className="mt-16">
+            {children}
+          </main>
         </Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import {Button} from '@nextui-org/button'
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    return (
+        <>
+    
+        </>
   );
 }
